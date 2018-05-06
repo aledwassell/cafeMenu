284f8e7dfd83ac23e7bbefb2a44720334e0bbe43
aledwassell <aledwassell@googlemail.com>
2018-05-06 09:40:44 +0100

    sorting rolover images

--- public/js/angApp.js
+++ public/js/angApp.js
@@ -55,22 +55,48 @@
             $scope.showImages = function ($index) {
                 $scope.selectedIndex = $index;
             }
-
-            $scope.breakfastBackground = setOrganiser.getUrls(['breakfast']);
-            $scope.lunchBackground = setOrganiser.getUrls(['dinner']);
-            $scope.dinnerBackground = setOrganiser.getUrls(['dinner']);
-            $q.all([
-                $scope.breakfastBackground.$promise,
-                $scope.lunchBackground.$promise,
-                $scope.dinnerBackground.$promise
-            ]).then(function() {
+            //
+            // $scope.breakfastBackground = setOrganiser.getUrls(['breakfast']);
+            // $scope.lunchBackground = setOrganiser.getUrls(['dinner']);
+            // $scope.dinnerBackground = setOrganiser.getUrls(['dinner']);
+            // $q.all([
+            //     $scope.breakfastBackground.$promise,
+            //     $scope.lunchBackground.$promise,
+            //     $scope.dinnerBackground.$promise
+            // ]).then(function() {
                 $scope.links = [
-                    {id: 1, url: 'breakfast', name: 'Breakfast', class:'breakfast', ctrl: 'breakfastCtrl', background: $scope.breakfastBackground.photoset.photo["0"].url_m},
-                    {id: 2, url: 'lunch', name: 'Lunch', class:'lunch', ctrl: 'lunchCtrl', background: $scope.lunchBackground.photoset.photo["0"].url_m},
-                    {id: 3, url: 'dinner', name: 'Dinner', class:'dinner', ctrl: 'dinnerCtrl', background: $scope.dinnerBackground.photoset.photo["0"].url_m},
-                    {id: 4, url: 'blog', name: 'Blog', class:'blog', ctrl: 'blogCtrl'}
+                    {
+                        id: 1,
+                        url: 'breakfast',
+                        name: 'Breakfast',
+                        class:'breakfast',
+                        ctrl: 'breakfastCtrl',
+                        // background: $scope.breakfastBackground.photoset.photo["0"].url_m
+                    },
+                    {
+                        id: 2,
+                        url: 'lunch',
+                        name: 'Lunch',
+                        class:'lunch',
+                        ctrl: 'lunchCtrl',
+                        // background: $scope.lunchBackground.photoset.photo["0"].url_m
+                    },
+                    {
+                        id: 3,
+                        url: 'dinner',
+                        name: 'Dinner',
+                        class:'dinner',
+                        ctrl: 'dinnerCtrl',
+                        // background: $scope.dinnerBackground.photoset.photo["0"].url_m
+                    },
+                    {
+                        id: 4,
+                        url: 'blog',
+                        name: 'Blog',
+                        class:'blog',
+                        ctrl: 'blogCtrl'}
                 ];
-            });
+            // });
 
         }])
 
